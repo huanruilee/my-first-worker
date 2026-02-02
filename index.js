846b4e6d519d095cb0f7230153e02e222ee63e7a
李桓瑞 <henry@lihuanruideMacBook-Pro.local>
2026-02-02 15:24:45 +0800

    Update from Mac

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ export default {
       <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>我的個人展示頁面</title>
+        <title>Henry 我的個人展示頁面</title>
         <style>
           body { 
             margin: 0; font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, sans-serif;
